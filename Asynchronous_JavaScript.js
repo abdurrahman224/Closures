495b08b0102de05800abec3a761575001a8a848a
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-10-02 09:39:04 +0600

    promise

--- Asynchronous_JavaScript.js
+++ Asynchronous_JavaScript.js
@@ -1,26 +1,108 @@
 
 
-const dependent = true;
+// const dependent = true;
 
-function simple(callback) {
-  console.log("Step 1: Start");
+// function simple(callback) {
+//   console.log("Step 1: Start");
 
-  if (dependent) callback();
+//   if (dependent) callback();
+// }
+
+// function delay(callback) {
+//   setTimeout(function () {
+//     console.log("Step 2: Loading...");
+//     callback();
+//   }, 2000);
+// }
+
+// function delayTime() {
+//   setTimeout(function () {
+//     console.log("Step 3: Done!");
+//   }, 1000);
+// }
+
+// simple(function () {
+//   delay(delayTime);
+// });
+
+
+
+const orderNow = true;
+
+function bookOrder() {
+  console.log("You con Books Order");
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("Order Placed");
+      
+      if (orderNow) {
+        resolve("Order Placed");
+      } else {
+        reject("error");
+      }
+    },100);
+  });
+  return promise;
+}
+function orderProceing() {
+
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log('Processing Order');
+      
+     if(true){
+             
+
+      resolve("Processing Order");
+     }else{
+      reject("Processing Failed")
+     }
+      
+    },1000);
+  });
+  return promise;
 }
 
-function delay(callback) {
-  setTimeout(function () {
-    console.log("Step 2: Loading...");
-    callback();
-  }, 2000);
+function processingShipped() {
+  const promise = new Promise(function (resolve, reject) {
+  
+    
+   setTimeout (function(){
+      console.log('Shipped');
+    resolve("Shipped");
+   
+
+   },1000)
+  });
+  return promise;
 }
 
-function delayTime() {
-  setTimeout(function () {
-    console.log("Step 3: Done!");
-  }, 1000);
+function delivered() {
+  const promise = new Promise(function (resolve, reject) {
+
+
+
+
+  setTimeout(function(){
+    console.log('delivered');
+
+resolve("delivered")
+
+  },1000)
+   
+  });
+  return promise;
 }
 
-simple(function () {
-  delay(delayTime);
-});
+bookOrder()
+  .then(orderProceing)
+  .then(processingShipped)
+  .then(delivered)
+  .then(function (value) {
+    console.log("falin : "+value);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
